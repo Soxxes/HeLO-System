f5425068313d840268de089ec6bb983f122b650c
Soxxes <50017993+Soxxes@users.noreply.github.com>
2021-09-02 17:13:42 +0200

    added basic database stuff

---
+++ data base/test.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import certifi
+
+#client = MongoClient("mongodb+srv://Marc:<password>@cluster0.t145g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#client = MongoClient("mongodb+srv://Marc:jK5%oWq@cluster0.t145g.mongodb.net/gettingStarted?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+client = MongoClient("mongodb+srv://Marc:jK5%oWq@cluster0.t145g.mongodb.net/gettingStarted?retryWrites=true&w=majority")
+# creates a new database on the cluster
+db = client.gettingStarted
+
+# creates a new collection
+scores = db.scores
+
+# first document to insert into the collection
+# first_doc = {
+#     "name": "38.",
+#     "auth": "63817",
+#     "score": 575
+# }
+
+# insert doc to collection
+# scores.insert_one(first_doc)
+
+# access docs
+for doc in db.scores.find():
+    print(doc["name"])
