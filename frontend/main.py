71a931a7ef6f7884312fd44c645b75d7c5c274ce
Soxxes <50017993+Soxxes@users.noreply.github.com>
2021-09-02 15:19:15 +0200

    basic UI done
    still ugly af

--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 
 
 class MyBoxLayout(BoxLayout):
@@ -10,7 +11,22 @@ class MyBoxLayout(BoxLayout):
 
 
 class MainWidget(Widget):
-    pass
+    helo_score_team1 = StringProperty("")
+    helo_score_team2 = StringProperty("")
+    
+    def _get_scores(self, name1, name2):
+        # get HeLO scores here
+        pass
+
+    def set_scores_label(self, name1, name2):
+        # get scores here
+        # s1, s2 = self._get_scores(name1, name2)
+        self.helo_score_team1 = "521" # s1
+        self.helo_score_team2 = "125" # s2
+
+    def calc_and_send(self, name1, name2, auth):
+        # get scores here
+        pass
 
 
 class HeLOApp(App):
