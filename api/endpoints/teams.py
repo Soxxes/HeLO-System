a51ce0249f3a5fdb0f7220a279e3c0927162a159
Soxxes <hscschwimmermarc@t-online.de>
2021-12-15 20:50:40 +0100

    implemented first GET attempt

--- api/endpoints/teams.py
+++ api/endpoints/teams.py
@@ -2,15 +2,29 @@
 Endpoint for the different teams.
 """
 
+from typing_extensions import Required
 from flask_restful import Resource, Api, reqparse
 
 class Teams(Resource):
 
     def get(self):
-        pass
+        parser = reqparse.RequestParser()
+
+        # required arguments for getting a team
+        parser.add_argument('name', required=True)
+
+        # parse arguments to dictionary
+        args = parser.parse_args()
+
+        # data base stuff
+        print(args["name"])
+
+        return "Hello", 200
+
 
     def post(self):
         pass
+
 
     def put(self):
         pass
